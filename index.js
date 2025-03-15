953ae5818d975ede8743cdc8e77a5bcd0be30795
Rituraj67 <rituraj672000@gmail.com>
2025-03-16 00:58:03 +0530

    lambda_yaml and serverless_yaml

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import serverless from 'serverless-http';
 
 import sequelize from './config/db.js'; // Sequelize instance
 import authRoutes from './routes/authRoutes.js';
@@ -30,19 +31,39 @@ app.use('/api/contact', contactRoutes);
 app.get("/", (req, res)=>{
   res.send("Express App is Running...");
 })
+app.get("/health", (req, res)=>{
+  res.send("Express App is Running...");
+})
 
-// Test DB connection
-try {
-  await sequelize.authenticate();
-  console.log('✅ PostgreSQL connected via Sequelize'); 
+// // Test DB connection
+// try {
+//   await sequelize.authenticate();
+//   console.log('✅ PostgreSQL connected via Sequelize'); 
 
-  await sequelize.sync({ alter: true, force: false }); // `force: false` by default
-  console.log('✅ Database synced');
-} catch (err) {
-  console.error('❌ Unable to connect or sync DB:', err);
+//   await sequelize.sync({ alter: true, force: false }); // `force: false` by default
+//   console.log('✅ Database synced');
+// } catch (err) {
+//   console.error('❌ Unable to connect or sync DB:', err);
+// }
+
+// Only initialize DB when NOT in Lambda
+if (process.env.IS_OFFLINE || process.env.NODE_ENV !== 'production') {
+  (async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('✅ PostgreSQL connected via Sequelize');
+      await sequelize.sync({ alter: true });
+      console.log('✅ Database synced');
+    } catch (err) {
+      console.error('❌ DB error:', err);
+    }
+  })();
 }
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
+
+export const handler = serverless(app);
+
